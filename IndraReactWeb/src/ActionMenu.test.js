7fc71a09bcc37d8cdbf64ffd32a60300d0bac3a5
Gene Callahan <gcallah@mac.com>
2021-05-06 14:10:31 -0400

    Added github push to web's `make prod`.

--- IndraReactWeb/src/ActionMenu.test.js
+++ IndraReactWeb/src/ActionMenu.test.js
@@ -3,21 +3,15 @@
 //import ActionMenu from './components/ActionMenu';
 
 describe('Mounted ActionMenu', () => {
-<<<<<<< HEAD
-=======
   // FIXME: need to add new tests for the new ActionMenu
->>>>>>> 8215dec6543d4e0eefeacfe332ac37cf3bf9ddb7
   //const wrapper = mount(<ActionMenu />);
   it('should render Population Graph on Click', () => {
     // const button = wrapper.find('btn-success').first();
     // button.simulate('click');
-<<<<<<< HEAD
     // const state = wrapper.state('loadingSourceCode');
     // expect(state).toEqual(false);
     // FIXME: need to add new test for the new ActionMenu
-=======
     //const state = wrapper.state('loadingSourceCode');
     //expect(state).toEqual(false);
->>>>>>> 8215dec6543d4e0eefeacfe332ac37cf3bf9ddb7
   });
 });
