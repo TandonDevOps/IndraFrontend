c20738d0ef2d9dac82265a35fedb32c84eb8ff89
gcallah <gcallah@mac.com>
2021-06-02 10:41:07 -0400

    Added PORT var and else message.

--- IndraReactWeb/local.sh
+++ IndraReactWeb/local.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-# See if something is already running on our port:
-export server=$(ps -ef | grep "[p]ort=8000")
+# See if something is already running on our port:false
+export API_PORT=8000
+# look for a process running on $PORT:
+export server=$(ps -ef | grep "[p]ort=$API_PORT")
+echo "Server = $server"
 if [ -z "$server" ]
 then
     # run local server in the background:
     echo "No running Indra API server detected: launching."
     cd $INDRA_HOME/APIServer && ./api.sh &
+else
+    echo "A server is already running on port $API_PORT"
 fi
 
 # run react locally against local server
-export REACT_APP_API_URL=http://127.0.0.1:8000/ 
+export REACT_APP_API_URL=http://127.0.0.1:$API_PORT/ 
 npm start
 echo "If you need to kill the API Server, you can run the ps command and kill its PID."
