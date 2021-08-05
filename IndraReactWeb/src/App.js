0210a55530078b50423fc836ef8a880691b348d0
Ayden <zw1460@nyu.edu>
2021-08-05 19:03:01 -0400

    Disable browser back button.

--- IndraReactWeb/src/App.js
+++ IndraReactWeb/src/App.js
@@ -9,6 +9,7 @@ import RunModel from './components/RunModel';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorCatching from './components/ErrorCatching';
 import DebugMenu from './components/DebugMenu';
+import autoBind from 'react-autobind';
 
 // The styling below should be in a style sheet, not in javascript:
 const Wrapper = styled('div')`
@@ -21,30 +22,46 @@ const Wrapper = styled('div')`
   }
 `;
 
-function App() {
-  return (
-    <Wrapper>
-      <HashRouter>
-        <Layout>
-          <IndraRoutes />
-        </Layout>
-      </HashRouter>
-    </Wrapper>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    autoBind(this);
+  }
+
+  componentDidMount() {
+    window.history.pushState(null, document.title, window.location.href);
+    window.addEventListener('popstate', function (event){
+      window.history.pushState(null, document.title,  window.location.href);
+    });
+  }
+
+  indraRoutes() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/wip" component={WIP} />
+        <Route exact path="/models/props/:id" component={ModelParams} />
+        <Route exact path="/models/menu/:id" component={RunModel} />
+        <Route exact path="/models/debug/:id" component={DebugMenu} />
+        <Route exact path="/errorCatching" component={ErrorCatching} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    );
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <HashRouter>
+          <Layout>
+            {this.indraRoutes()}
+          </Layout>
+        </HashRouter>
+      </Wrapper>
+    );
+  }
 }
 
-export function IndraRoutes() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/wip" component={WIP} />
-      <Route exact path="/models/props/:id" component={ModelParams} />
-      <Route exact path="/models/menu/:id" component={RunModel} />
-      <Route exact path="/models/debug/:id" component={DebugMenu} />
-      <Route exact path="/errorCatching" component={ErrorCatching} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  );
-}
 
 export default withTheme(App);
