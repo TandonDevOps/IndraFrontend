226c81116edf0b9698f7c394e923fa67c58deff4
jingyi <jl11525@nyu.edu>
2021-11-29 01:41:46 +0800

    [model-generator] feat: invoke create_action api

--- IndraReactWeb/src/components/ModelGenerator.js
+++ IndraReactWeb/src/components/ModelGenerator.js
@@ -127,11 +127,12 @@ export default () => {
   );
 
   const [action, setAction] = useState("");
+  const [actionName, setActionName] = useState("");
   const [loadingStepThree, setLoadingStepThree] = useState(false);
 
   const modelAction = () => {
     setLoadingStepThree(true);
-    axios.post(`${config.GENERATOR_CREATE_ACTION}${secondData && secondData.exec_key}?group_name=${action}`)
+    axios.post(`${config.GENERATOR_CREATE_ACTION}${secondData && secondData.exec_key}?group_name=${action}&group_action=${actionName}`)
       .then((res) => {
         setLoadingStepThree(false);
         console.log(res.data);
@@ -187,7 +188,7 @@ export default () => {
           </Dropdown.Menu>
         </Dropdown>
         <div>Describe the group action</div>
-        <input />
+        <input onChange={e => setActionName(e.target.value)} />
         <br />
         <button
           className="btn btn-success m-1"
